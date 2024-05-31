4f85492967af1f737535a9b6ab00fbb54759069b
bagoesanugrah <bagoesanugrah69@gmail.com>
2024-05-31 19:38:16 +0700

    update 310524

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -35,7 +35,7 @@ class Database extends Config
 		'hostname' => 'localhost',
 		'username' => 'root',
 		'password' => '',
-		'database' => 'pemesananmobil',
+		'database' => 'pemesananmobil3',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => '',
 		'pConnect' => false,
